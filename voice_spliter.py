1021a60d3685652205703a0ccd9d6adced6ce123
katgiadla <Kasia1997>
2021-05-11 16:13:04 +0200

    Change a conception of program

--- voice_spliter.py
+++ voice_spliter.py
@@ -1,13 +1,4 @@
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
-
-def check_file(path: str) -> AudioSegment:
-    ext = path.split(".")[1]
-    return {
-        "wav": lambda path: AudioSegment.from_wav(path),
-        "mp3": lambda path: AudioSegment.from_mp3(path)}.get(ext)(path)
 
 
-def split_voice(path: str):
-    record = check_file()
-
+if __name__ == "__main__":
+    print("coś")
