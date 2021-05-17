9e81c8e5361e493c2457f4e10018971968f264be
katgiadla <Kasia1997>
2021-05-17 21:47:24 +0200

    Add functions for splitting and cleaning words from transcription

--- tests/transcription_loader_tests.py
+++ tests/transcription_loader_tests.py
@@ -1,28 +1,49 @@
 import unittest
-
-from docx.opc.exceptions import PackageNotFoundError
-
-from transcription_loader import open_docx_transcription, open_txt_transcription
+from exceptions import EmptyListError
+from transcription_loader import *
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.testpathtodocx = 'D:\\preparator_voices\\test_files\\test.docx'
-        self.failedtestpathtodocx = 'D:\\preparator_voices\\test_files\\test1.docx'
-        self.testpathtotxt = 'D:\\preparator_voices\\test_files\\test.txt'
-        self.failedtestpathtotxt = 'D:\\preparator_voices\\test_files\\test1.txt'
+        self.test_path_to_docx = 'D:\\preparator_voices\\test_files\\test.docx'
+        self.test_failed_path_to_docx = 'D:\\preparator_voices\\test_files\\test1.docx'
+        self.test_path_to_txt = 'D:\\preparator_voices\\test_files\\test.txt'
+        self.test_failed_path_to_txt = 'D:\\preparator_voices\\test_files\\test1.txt'
+        self.test_empty_transcription = []
+        self.test_one_line_transcription_with_special_chars = ['Witaj, bo przemądrzali stali się głupcami\n']
+        self.test_one_line_transcription_without_special_chars = ['Witaj raju pragnących zbawienia']
+        self.test_two_lines_transcription = ['Policjanci nie przebywają z politykami na codzień', 'Szyby, skrobiemy']
 
     def test_docx_document_not_exist(self):
-        self.assertRaises(FileNotFoundError, open_docx_transcription, self.failedtestpathtodocx)
+        self.assertRaises(FileNotFoundError, open_docx_transcription, self.test_failed_path_to_docx)
 
     def test_docx_document_exists(self):
-        self.assertListEqual(['To jest przykładowy tekst'], open_docx_transcription(self.testpathtodocx))
+        self.assertListEqual(['To jest przykładowy tekst'], open_docx_transcription(self.test_path_to_docx))
 
     def test_txt_document_not_exist(self):
-        self.assertRaises(FileNotFoundError, open_txt_transcription, self.failedtestpathtotxt)
+        self.assertRaises(FileNotFoundError, open_txt_transcription, self.test_failed_path_to_txt)
 
     def test_txt_document_exists(self):
         self.assertListEqual(['Nie prowadzimy psychoterapii\n', 'no Twój kuzyn stanowisko dostał umlaüt'],
-                             open_txt_transcription(self.testpathtotxt))
+                             open_txt_transcription(self.test_path_to_txt))
+
+    def test_split_transcriptions_lines_list_empty_list(self):
+        self.assertRaises(EmptyListError.EmptyListError, split_lines_to_words, [])
+
+    def test_split_transcription_lines_one_line_without_special_chars(self):
+        self.assertListEqual(['Witaj', 'raju', 'pragnących', 'zbawienia'],
+                             split_lines_to_words(self.test_one_line_transcription_without_special_chars))
+
+    def test_split_transcription_lines_one_line_with_special_chars(self):
+        self.assertListEqual(['Witaj,', 'bo', 'przemądrzali', 'stali', 'się', 'głupcami\n'],
+                             split_lines_to_words(self.test_one_line_transcription_with_special_chars))
+
+    def test_split_transcription_lines_more_lines(self):
+        self.assertListEqual(['Policjanci', 'nie', 'przebywają', 'z', 'politykami', 'na', 'codzień', 'Szyby,', 'skrobiemy'],
+                             split_lines_to_words(self.test_two_lines_transcription))
+
+    def test_cleaning_more_whitespace(self):
+        self.assertListEqual(['Witaj', 'bo', 'przemądrzali', 'stali', 'się', 'głupcami'],
+                             cleaning_transcription_list(['Witaj,', 'bo', 'przemądrzali', 'stali', 'się', 'głupcami\n']))
 
 if __name__ == '__main__':
     unittest.main()
--- transcription_loader.py
+++ transcription_loader.py
@@ -1,4 +1,8 @@
+import re
+from itertools import chain
 from docx import Document
+from exceptions import EmptyListError
+
 
 def open_docx_transcription(path: str) -> list:
     """
@@ -50,6 +54,46 @@ def open_txt_transcription(path: str) -> list:
         raise
 
     return transcription_list
+
+def split_lines_to_words(transcriptions_lines: list) -> list:
+    """
+    Divide lines of transcriptions by the words
+    If list of lines is empty, EmptyListError has been thrown
+    Args:
+        transcriptions_lines (list): lines of transcription
+
+    Returns:
+        words (list):   list of separated words
+    """
+    words = []
+
+    if not transcriptions_lines:
+        raise EmptyListError.EmptyListError
+    else:
+        for counter, line in enumerate(transcriptions_lines):
+            words.append(line.split(' '))
+
+    words = list(chain(*words))
+    return words
+
+def cleaning_transcription_list(transcription_words: list) -> list:
+    """
+    Cleaning from whitespaces and preparing list of words from transcriptions.
+    If transcription is empty function throws EmptyListError
+
+    Args:
+        transcription_words (list):  list of words in documents
+
+    Returns:
+        transcript_words (list):    clean words from transcription
+    """
+    if not transcription_words:
+        raise EmptyListError.EmptyListError
+    else:
+        for counter, word in enumerate(transcription_words):
+            transcription_words[counter] = str(word).strip('\n\t\.\,')
+
+    return transcription_words
 
 def check_type_of_description(path: str):
     scenario = 0
